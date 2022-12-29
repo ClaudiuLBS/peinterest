46c1816ef8988b6e5c63eedf6b587fb4b06f3510
Tanase Flavian <flaviantanase30@gmail.com>
2022-12-29 17:24:31 +0200

    Added AdminController

--- project.net/Controllers/AdminPanelController.cs
+++ project.net/Controllers/AdminPanelController.cs
@@ -1,20 +1,180 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using project.net.Data;
+using project.net.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using project.net.Data;
-using project.net.Models;
 
 namespace project.net.Controllers
 {
-    [Authorize(Roles ="Admin")] 
+    [Authorize(Roles = "Admin")]
+    [Route("/Admin")]
     public class AdminPanelController : Controller
     {
         private readonly ApplicationDbContext db;
 
-        private readonly UserManager<AppUser> 
+        private readonly UserManager<AppUser> _userManager;
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+
+        public AdminPanelController(
+               ApplicationDbContext context,
+               UserManager<AppUser> userManager,
+               RoleManager<IdentityRole> roleManager
+               )
+        {
+            db = context;
+
+            _userManager = userManager;
+
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var users = from user in db.Users
+                        orderby user.UserName
+                        select user;
+
+            ViewBag.UsersList = users;
+
+            return View();
+        }
+
+        public async Task<ActionResult> Show(string id)
+        {
+            AppUser user = db.Users.Find(id);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            ViewBag.Roles = roles;
+
+            return View(user);
+        }
+
+        public async Task<ActionResult> Edit(string id)
+        {
+            AppUser user = db.Users.Find(id);
+
+            user.AllRoles = GetAllRoles();
+
+            var roleNames = await _userManager.GetRolesAsync(user); // Lista de nume de roluri
+
+            // Cautam ID-ul rolului in baza de date
+            var currentUserRole = _roleManager.Roles
+                                              .Where(r => roleNames.Contains(r.Name))
+                                              .Select(r => r.Id)
+                                              .First(); // Selectam 1 singur rol
+            ViewBag.UserRole = currentUserRole;
+
+            return View(user);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Edit(string id, AppUser newData, [FromForm] string newRole)
+        {
+            AppUser user = db.Users.Find(id);
+
+            user.AllRoles = GetAllRoles();
+
+
+            if (ModelState.IsValid)
+            {
+                user.UserName = newData.UserName;
+                user.Email = newData.Email;
+                user.FirstName = newData.FirstName;
+                user.LastName = newData.LastName;
+                user.PhoneNumber = newData.PhoneNumber;
+
+
+                // Cautam toate rolurile din baza de date
+                var roles = db.Roles.ToList();
+
+                foreach (var role in roles)
+                {
+                    // Scoatem userul din rolurile anterioare
+                    await _userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                // Adaugam noul rol selectat
+                var roleName = await _roleManager.FindByIdAsync(newRole);
+                await _userManager.AddToRoleAsync(user, roleName.ToString());
+
+                db.SaveChanges();
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            var user = db.Users
+                         .Include("Bookmarks")
+                         .Include("Comments")
+                         .Include("Upvotes")
+                         .Include("BookmarkCategories")
+                         .Where(u => u.Id == id)
+                         .First();
+
+            // Delete user bookmarks
+            if (user.Bookmarks.Count > 0)
+            {
+                foreach (var bookmark in user.Bookmarks)
+                {
+                    db.Bookmarks.Remove(bookmark);
+                }
+            }
+            // Delete user comments
+            if (user.Comments.Count > 0)
+            {
+                foreach (var comment in user.Comments)
+                {
+                    db.Comments.Remove(comment);
+                }
+            }
+            // Delete user upvotes 
+            if (user.Upvotes.Count > 0)
+            {
+                foreach (var upvote in user.Upvotes)
+                {
+                    db.Upvotes.Remove(upvote);
+                }
+            }
+            // Delete user saved bookmarks
+            /*
+            if (user.BookmarkCategories.Count > 0)
+            {
+                foreach (var savedBookmark in user.BookmarkCategories)
+                {
+                    db.BookmarkCategories.Remove(savedBookmark);
+                }
+            }
+            */
+
+            db.AppUsers.Remove(user);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        [NonAction]
+        public IEnumerable<SelectListItem> GetAllRoles()
+        {
+            var selectList = new List<SelectListItem>();
+
+            var roles = from role in db.Roles
+                        select role;
+
+            foreach (var role in roles)
+            {
+                selectList.Add(new SelectListItem
+                {
+                    Value = role.Id.ToString(),
+                    Text = role.Name.ToString()
+                });
+            }
+            return selectList;
+        }
     }
 }

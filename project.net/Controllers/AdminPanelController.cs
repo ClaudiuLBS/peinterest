ba53eed0f281e547dbd9fcc89a12bc8814365e26
Tanase Flavian <flaviantanase30@gmail.com>
2022-12-29 15:35:07 +0200

    Added admin panel

---
+++ project.net/Controllers/AdminPanelController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using project.net.Data;
+using project.net.Models;
+
+namespace project.net.Controllers
+{
+    [Authorize(Roles ="Admin")] 
+    public class AdminPanelController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        private readonly UserManager<AppUser> 
+    }
+}
